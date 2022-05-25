77051f291e2a111d7327c9ff3ad2c9ed45d4d119
Aditya Raj <aditya8raj000@gmail.com>
2022-05-25 20:48:36 +0530

    added a make friend button

--- src/Card.js
+++ src/Card.js
@@ -1,66 +1,104 @@
 import React from 'react'
 
+const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+
+const alert = (message, type) => {
+    const wrapper = document.createElement('div')
+    wrapper.innerHTML = [
+        `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+        `   <div>${message}</div>`,
+        '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+        '</div>'
+    ].join('')
+
+    alertPlaceholder.append(wrapper)
+}
+
+const alertTrigger = document.getElementById('liveAlertBtn')
+if (alertTrigger) {
+    alertTrigger.addEventListener('click', () => {
+        alert("Thanks for making me your friend", "success")
+    })
+}
 export default function Card(props) {
     return (
         <div className="flex flex-wrap  justify-center">
             <div className="br4 ma3 ba bw1 w-25 pa3 bg-black grow shadow-2 tc">
                 <img src={require('./first.png')} alt="first-img" />
-                <h2>{props.name1}</h2>
-                <p>{props.email1}</p>
+                <h4>{props.name1}</h4>
+                <div id="liveAlertPlaceholder"></div>
+                <button type="button" className="btn btn-primary" id="liveAlertBtn">Make Friend</button>
             </div>
 
             <div className="br4 ma3 ba bw1 w-25 pa3 bg-black grow shadow-2 tc">
                 <img src={require('./second.png')} alt="second-img" />
-                <h2>{props.name2}</h2>
-                <p>{props.email2}</p>
+                <h4>{props.name2}</h4>
+                <div id="liveAlertPlaceholder"></div>
+                <button type="button" className="btn btn-primary" id="liveAlertBtn">Make Friend</button>
             </div>
 
             <div className="br4 ma3 ba bw1 w-25 pa3 bg-black grow shadow-2 tc">
                 <img src={require('./third.png')} alt="third-img" />
-                <h2 >{props.name3}</h2>
-                <p >{props.email3}</p>
+                <h4 >{props.name3}</h4>
+
+                <div id="liveAlertPlaceholder"></div>
+                <button type="button" className="btn btn-primary" id="liveAlertBtn">Make Friend</button>
             </div>
 
             <div className="br4 ma3 ba bw1 w-25 pa3 bg-black grow shadow-2 tc">
                 <img src={require('./fourth.png')} alt="fourth-img" />
-                <h2 >{props.name4}</h2>
-                <p >{props.email4}</p>
+                <h4 >{props.name4}</h4>
+
+                <div id="liveAlertPlaceholder"></div>
+                <button type="button" className="btn btn-primary" id="liveAlertBtn">Make Friend</button>
             </div>
 
             <div className="br4 ma3 ba bw1 w-25 pa3 bg-black grow shadow-2 tc">
                 <img src={require('./fifth.png')} alt="fifth-img" />
-                <h2 >{props.name5}</h2>
-                <p >{props.email5}</p>
+                <h4 >{props.name5}</h4>
+
+                <div id="liveAlertPlaceholder"></div>
+                <button type="button" className="btn btn-primary" id="liveAlertBtn">Make Friend</button>
             </div>
 
             <div className="br4 ma3 ba bw1 w-25 pa3 bg-black grow shadow-2 tc">
                 <img src={require('./sixth.png')} alt="sixth-img" />
-                <h2 >{props.name6}</h2>
-                <p >{props.email6}</p>
+                <h4 >{props.name6}</h4>
+
+                <div id="liveAlertPlaceholder"></div>
+                <button type="button" className="btn btn-primary" id="liveAlertBtn">Make Friend</button>
             </div>
 
             <div className="br4 ma3 ba bw1 w-25 pa3 bg-black grow shadow-2 tc">
                 <img src={require('./seventh.png')} alt="seventh-img" />
-                <h2 >{props.name7}</h2>
-                <p >{props.email7}</p>
+                <h4 >{props.name7}</h4>
+
+                <div id="liveAlertPlaceholder"></div>
+                <button type="button" className="btn btn-primary" id="liveAlertBtn">Make Friend</button>
             </div>
 
             <div className="br4 ma3 ba bw1 w-25 pa3 bg-black grow shadow-2 tc">
                 <img src={require('./eighth.png')} alt="eighth-img" />
-                <h2 >{props.name8}</h2>
-                <p >{props.email8}</p>
+                <h4 >{props.name8}</h4>
+
+                <div id="liveAlertPlaceholder"></div>
+                <button type="button" className="btn btn-primary" id="liveAlertBtn">Make Friend</button>
             </div>
 
             <div className="br4 ma3 ba bw1 w-25 pa3 bg-black grow shadow-2 tc">
                 <img src={require('./ninth.png')} alt="ninth-img" />
-                <h2 >{props.name9}</h2>
-                <p >{props.email9}</p>
+                <h4 >{props.name9}</h4>
+
+                <div id="liveAlertPlaceholder"></div>
+                <button type="button" className="btn btn-primary" id="liveAlertBtn">Make Friend</button>
             </div>
 
             <div className="br4 ma3 ba bw1 w-25 pa3 bg-black grow shadow-2 tc">
                 <img src={require('./tenth.png')} alt="tenth-img" />
-                <h2 >{props.name10}</h2>
-                <p >{props.email10}</p>
+                <h4 >{props.name10}</h4>
+
+                <div id="liveAlertPlaceholder"></div>
+                <button type="button" className="btn btn-primary" id="liveAlertBtn">Make Friend</button>
             </div>
         </div>
 
