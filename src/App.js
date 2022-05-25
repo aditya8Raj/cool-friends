31e4f32f82dd7af0086c6e0d698d3bb10bc6fc13
Aditya Raj <aditya8raj000@gmail.com>
2022-05-26 00:43:39 +0530

    changed fifth image

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-// import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 import Cardlist from './Cardlist';
@@ -6,14 +5,13 @@ import Searchbox from './Searchbox';
 // import 'tachyons';
 
 
-// const [first, setfirst] = useState(second)
 
 export default function App() {
   return (
     <>
       <div>
         <header className="white-80 tc pv1 avenir">
-          <h1 className="mt2 mb0 fw1 f1">COOL FRIENDS</h1>
+          <h1 className="mt2 mb0 fw1 f1 font-link">COOL FRIENDS</h1>
           <br />
           <Searchbox />
         </header>
